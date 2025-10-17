d9749cc3cd21b895c739395f29f13bfaabf675e4
micheal <944917574@qq.com>
2025-10-17 11:56:31 +0800

    20251017

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -10,22 +10,37 @@
 #include "test_mm.h"
 
 void kmain() {
-
+    fb_init();
     serial_init(COM1,1);          // 初始化串口                                                                                               
     serial_write(COM1, "MatrixOS Booting...\n");
+    fb_puts("MatrixOS Booting...\n");
 
     // 初始化内存管理系统                                                                                                                     
     pmm_init(0x1000000, 0x2000000);  // 16MB - 32MB                                                                                          
     temp_mapping_init();
     kmalloc_init();
 
-    // 运行所有测试                                                                                                                           
-    run_all_memory_tests();
-
     serial_printf(COM1, "\nSystem ready for normal operation.\n");
 
-    while(1) {
-        asm volatile("hlt");
+    isr_install();              // 安装中断
+    
+    // 开启全局中断
+
+    __asm__ volatile ("sti");   
+
+    keyboard_init();
+
+    serial_write(COM1, "Interrupts ENABLED!\n");
+    fb_puts("Interrupts ENABLED!\n");
+
+    while (1) {
+        char c = keyboard_get_char();
+        if (c){
+            serial_printf(COM1, "Main loop got char: '%c' '%x'\n", c, c);
+            // 可选：在屏幕上也回显
+            //char buf[2] = {c, 0};
+            fb_putchar(c);
+        }
     }
 }
 
